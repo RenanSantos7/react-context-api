28ec0bd565ec602d53236b9e35f301bd7a7d67d4
Renan Santos <renansantos7@proton.me>
2024-01-01 18:57:25 -0300

    iniciando em ContextAPI

---
+++ src/context/Carrinho.jsx
@@ -0,0 +1,11 @@
+import { createContext } from "react"
+
+export const CarrinhoContext = createContext()
+
+export function CarrinhoProvider({children}) {
+    return (
+        <CarrinhoContext.Provider>
+            {children}
+        </CarrinhoContext.Provider>
+    )
+}
